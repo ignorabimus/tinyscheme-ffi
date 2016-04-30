def065e912db58e04ebdd26c1a513c1ea54d01eb
Tatsuya Watanabe <watanabe@ignorabimus.info>
2016-04-30 19:40:43 +0900

    Add libffi static-library (libffi.lib) and some headers (ffi.h, ffitarget.h)

--- msvcbuild.bat
+++ msvcbuild.bat
@@ -1,6 +1,6 @@
 @setlocal
 @set SCHEME_H_DIR=..\src
-@set TSCOMPILE=cl /nologo /O2 /W3 /c /D_CRT_SECURE_NO_DEPRECATE /DWIN32 /DUSE_DL=1 /DFFI_BUILDING /I%SCHEME_H_DIR% /Ilibffi\src\x86
+@set TSCOMPILE=cl /nologo /O2 /W3 /c /D_CRT_SECURE_NO_DEPRECATE /DWIN32 /DUSE_DL=1 /DFFI_BUILDING /I%SCHEME_H_DIR% /Ilibffi\include
 @set TSLIB=lib /nologo
 @set TSLINK=link /nologo
 
@@ -10,10 +10,10 @@
 )
 
 %TSCOMPILE% /MT ffi.c
-%TSLINK% /DLL /out:dll\ffi.dll /export:init_ffi ffi.obj libffi.lib
+%TSLINK% /DLL /out:dll\ffi.dll /export:init_ffi ffi.obj libffi\libffi.lib
 
 %TSCOMPILE% /MDd ffi.c
-%TSLINK% /DLL /out:dll\ffi_d.dll /export:init_ffi ffi.obj libffi.lib
+%TSLINK% /DLL /out:dll\ffi_d.dll /export:init_ffi ffi.obj libffi\libffi_d.lib
 
 @goto END
 
@@ -23,10 +23,10 @@
 )
 
 %TSCOMPILE% /MT ffi.c
-%TSLIB% /out:lib\ffi.lib ffi.obj libffi.lib
+%TSLIB% /out:lib\ffi.lib ffi.obj libffi\libffi.lib
 
 %TSCOMPILE% /MDd ffi.c
-%TSLIB% /out:lib\ffi_d.lib ffi.obj libffi.lib
+%TSLIB% /out:lib\ffi_d.lib ffi.obj libffi\libffi_d.lib
 
 :END
 del ffi.obj
